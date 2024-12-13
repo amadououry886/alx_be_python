6e7640d929ecc7207158cc92898e70fc25758f0d
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2024-12-14 03:35:32 +0800

    Corrected arithmetic operations and print statements

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -8,7 +8,6 @@ substraction = number1 - number2
 multiplication = number1 * number2
 
 # Print results in a human-readable format
-print(f"Addition of {number1} and {number2} is {addition}) 
-    print(f"Substraction of {number1} and {number2} is {substration})
-print(f"multiplication of {number1} and {number2} is {multiplication})
-
+print(f"Addition of {number1} and {number2} is {addition}") 
+print(f"Substraction of {number1} and {number2} is {substraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")
