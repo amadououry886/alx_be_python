485fd43e12b771ccd71b3d819fe2c0a082b03b80
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2024-12-14 19:29:31 +0800

    I have change some

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+#Future_age_calculator.py
+
+#Prompt the user for their current age
+
+current_age = int(input("How old are you? "))
+
+# Calculate the user's age in the year 2050
+age_in_2050 = current_age + 27
+
+print(f"In 2050, you will be {age_in_2050} age old")
