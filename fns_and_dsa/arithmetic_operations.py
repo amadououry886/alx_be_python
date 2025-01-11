05127f93d40a1391cd1ad57b730d6f643f2c2467
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-01-12 04:19:51 +0800

    This is about operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,41 @@
+def perform_operation(num1, num2, operation):
+
+    if operation == 'add':
+        return num1 + num2 
+
+    elif operation == 'substract':
+        return num1 - num2
+    
+    elif operation == 'multiply':
+
+        return num1 * num2
+
+    elif operation == 'divide':
+
+        if num2 == 0:
+            return "cannot divide by Zero"
+        return num1/num2
+
+    else:
+       return "Invalid operation Please choose between : add, substract, multiply, divide"
+
+
+from arithmetic_operations import perform_operation
+
+def main():
+    print("Arithmetic Operations")
+
+    num1 = float(input("Enter the first number: "))
+    
+    num2 = float(input("Enter the second number: "))
+
+    operation = input("Enter the operation : add, substract, multiply, divide: ")
+
+    result = perform_operation(num1, num2, operation)
+
+    print(f"Result: {result}")
+
+if  __name__ == "__main__":
+
+    main()
+
