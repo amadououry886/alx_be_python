20206b772a2e7667045061c75ffe2f4c88d5de39
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-01-26 02:20:30 +0800

    Some changes

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,24 +1,26 @@
 class Book:
-
-    def __init__(self, title, autor, year):
-
+    def __init__(self, title, author, year):
+        """
+        Constructor to initialize a Book instance with title, author, and publication year.
+        """
         self.title = title
-        self.autor = autor
+        self.author = author
         self.year = year
 
-
     def __del__(self):
-
+        """
+        Destructor that prints a message when the Book object is deleted.
+        """
         print(f"Deleting {self.title}")
 
-
     def __str__(self):
+        """
+        String representation of the Book instance for user-friendly display.
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
 
-
-        return f"{self.title} by {self.autor}, published in {self.year}"
-
-
-
-    def __rpr__(self):
-
+    def __repr__(self):
+        """
+        Official representation of the Book instance for debugging and recreating the object.
+        """
         return f"Book('{self.title}', '{self.author}', {self.year})"
